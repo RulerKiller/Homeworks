b0ee1186c917833e03553be9b9fc6b38fb0bd86b
人工智能 北京师范大学 <mgyyh1651123@163.com>
2024-10-11 21:16:24 +0800

    脚踏实地

--- Homeworks/3.E.c
+++ Homeworks/3.E.c
@@ -2,6 +2,14 @@
 
 int main()
 {
-    printf("1.71828");
-    return 0;
+	int i, n = 1;
+	float sum = 0;
+	for (i = 1; i <= 10; i++)
+	{
+		n = n * i;
+		sum = sum + 1.0 / n;
+
+	}
+	printf("%.5f", sum);
+	return 0;
 }
--- Homeworks/3.H.c
+++ Homeworks/3.H.c
@@ -2,6 +2,22 @@
 
 int main()
 {
-    printf("34");
-    return 0;
+	int i, j, k, l, sum = 0;
+	for (i = 1; i <= 33; i++)
+	{
+		for (j = 1; j <= 40; j++)
+		{
+			for (k = 1; k <= 20; k++)
+			{
+				for (l = 1; l <= 10; l++)
+				{
+					if (i + j + k + l == 40 && i + 2 * j + 5 * k + 10 * l == 100)
+					{
+						sum = sum + 1;
+					}
+				}
+			}
+		}
+	}
+	printf("%d", sum);
 }
